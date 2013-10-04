450942a51e7e26f31cc2778712ca51365b809ddd
Nat <nat.wrw@gmail.com>
2013-10-04 11:01:20 +0700

    handle post request

--- app.js
+++ app.js
@@ -2,15 +2,17 @@ var connect = require('connect')
   , http = require('http');
 
 var app = connect()
+  .use(function(req, res, next){
+      req.method = "GET"; 
+      next();
+    //res.end('Hello from Connect!\n');
+  })
   .use(connect.favicon())
   .use(connect.logger('dev'))
   .use(connect.static('public'))
   .use(connect.directory('public'))
   .use(connect.cookieParser())
-  .use(connect.session({ secret: 'my secret here' }))
-  .use(function(req, res){
-    res.end('Hello from Connect!\n');
-  });
+  .use(connect.session({ secret: 'my secret here' }));
 
 var port = process.env.PORT || 5000;
 
